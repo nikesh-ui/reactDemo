4b4191c3ec69bceb3c7f561a838a5446cd65f820
Nikesh <subedi.nikesh@outlook.com>
2025-12-24 17:45:07 +0545

    [NET-3] form validatation and edit function

--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -1,15 +1,21 @@
 import { MOCK_PROJECTS } from "../Models/MockProject";
 import ProjectList from "./ProjectList";
 import { Project } from "../Models/Project";
+import { useState } from "react";
 
 function Projectspage() {
+  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+
   const saveProject = (project: Project) => {
-    console.log("Saving Project: ", project);
+    let updatedProjects = projects.map((p: Project) => {
+      return p.id === project.id ? project : p;
+    });
+    setProjects(updatedProjects);
   };
   return (
     <>
       <h1>Projects</h1>
-      <ProjectList onSave={saveProject} projects={MOCK_PROJECTS} />
+      <ProjectList onSave={saveProject} projects={projects} />
     </>
   );
 }
